3bdce9c1d508135bd3c8562c2be7e042b6875d45
Daniel Kasprów <127341@student.uni.opole.pl>
2021-03-21 15:23:37 +0100

    zad5 add Pesel,wiek,checking

--- Lista3/src/zad5/Przeciazanie.java
+++ Lista3/src/zad5/Przeciazanie.java
@@ -4,10 +4,31 @@ public class Przeciazanie {
 	
 	public static void Dane(String pesel) {
 	System.out.println("Twoja data urodzenia to 19" + pesel.substring(0, 2) +"." + pesel.substring(2, 4) +"."+pesel.substring(4, 6));
+	if(pesel.charAt(9)%2==0) 
+		System.out.println("Plec: Kobieta");
+	else
+		System.out.println("Plec: Mezczyzna");
 	}
 	
 	public static void Dane(long wiek) {
 	long r = 2021-wiek;
 	System.out.println("Urodziles sie w	" + r +	" roku");
+	}
+	
+	public static void Dane(String dane,int ifName)
+	{
+		
+	}
+	
+	public static void Dane(String dane,int ifDate, int ifName)
+	{
+		for(int x=0;x<dane.length();x++)
+		{
+			char myStrCharacter = dane.charAt(x);//przypisanie znaku
+			if(myStrCharacter == ' ')//sprzawdzanie czy dany znak slowa jest taki sam znak podslowa
+				ifName++;
+			if(myStrCharacter == '.')//sprzawdzanie czy dany znak slowa jest taki sam znak podslowa
+				ifDate++;
+		}
 	}
 }
--- Lista3/src/zad5/Rozpoznawanie.java
+++ Lista3/src/zad5/Rozpoznawanie.java
@@ -3,12 +3,33 @@ package zad5;
 public class Rozpoznawanie {
 	public static void main(String[] args) {
 		
-		String pesel ="25";
-		long wiek = Long.parseLong(pesel); 
+		String dane ="61110512143";
+		int ifDate = 0;
+		int ifName = 0;
+		Przeciazanie.Dane(dane,ifDate , ifName);
 		
-		if(pesel.length() == 11)
-			Przeciazanie.Dane(pesel);
-		else
-			Przeciazanie.Dane(wiek);
+		if(ifName == 0)
+		{
+			if(ifDate==0)
+			{
+				if(dane.length() == 11)
+				{
+					Przeciazanie.Dane(dane);
+				}
+				else
+				{
+					long wiek = Long.parseLong(dane); 
+					Przeciazanie.Dane(wiek);
+				}
+			}
+			else
+			{
+				
+			}
 		}
+		else
+		{
+			
+		}
+	}
 }
